224d40a515ecac78fdaac00f7dbefc63c274aeae
Sofia Gonzalez <sofi7zubieta@gmail.com>
2023-02-10 10:53:05 -0500

    fix

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,8 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.min.js";
 import "antd/dist/reset.css";
 
+import TopNav from "../components/TopNav"; //NAVBAR
+
 function MyApp ({Component, pageProps}){
-    return <Component {...pageProps} />;
+    return (
+    <>  
+        <TopNav/>
+        <Component {...pageProps} />;
+    </>
+    )
 }
 
 export default MyApp;
