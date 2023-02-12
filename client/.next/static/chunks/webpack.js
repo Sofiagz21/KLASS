e5c9a910a0bfe1ad64da026a89e233c86c9bc297
Sofia Gonzalez <sofi7zubieta@gmail.com>
2023-02-11 23:57:41 -0500

    fix

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec1123f31eba312c"; }
+/******/ 		__webpack_require__.h = function() { return "46420d7b42fce81d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
